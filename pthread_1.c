5dd7ca7f5298d36e26a7ad7f006bb8ac86e3c536
jahnjo <johndotfoster@gmail.com>
2018-04-12 16:01:26 -0400

    Finished product

--- pthread_1.c
+++ pthread_1.c
@@ -12,9 +12,7 @@ struct threadArgs {
 
 void* jerryTurn(void* arg) {
     struct threadArgs *jerryArgs = (struct threadArgs*)arg;
-    pthread_mutex_init(&(*jerryArgs).lock, NULL);
-    pthread_cond_init(&(*jerryArgs).jerry, NULL);
-    pthread_cond_init(&(*jerryArgs).tom, NULL);
+
 
 
     while ((*jerryArgs).stonesAvailable > 0) {
@@ -23,12 +21,15 @@ void* jerryTurn(void* arg) {
         while((*jerryArgs).turn == 1) {
             pthread_cond_wait(&(*jerryArgs).jerry, &(*jerryArgs).lock);
         }
-        
+        if ((*jerryArgs).stonesAvailable <= 0) {
+            printf("Jerry is the winner!\n");
+            pthread_exit(0);
+        }
         (*jerryArgs).stonesAvailable -= 1;
-        printf("Jerry picks up 1 stone, %d left\n",(*jerryArgs).stonesAvailable);
+        printf("Jerry picks up 1 stone, %d left\n\n",(*jerryArgs).stonesAvailable);
         (*jerryArgs).turn = 1;
         pthread_cond_signal(&(*jerryArgs).tom);
-        sleep(1);
+        //sleep(1);
         pthread_mutex_unlock(&(*jerryArgs).lock);
     }
 
@@ -39,21 +40,29 @@ void* jerryTurn(void* arg) {
 void* tomTurn(void* arg) {
     struct threadArgs *tomArgs = (struct threadArgs*)arg;
 
-    pthread_mutex_init(&(*tomArgs).lock, NULL);
-    pthread_cond_init(&(*tomArgs).jerry, NULL);
-    pthread_cond_init(&(*tomArgs).tom, NULL);
-
+    int stones;
 
     while ((*tomArgs).stonesAvailable > 0) {
         pthread_mutex_lock(&(*tomArgs).lock);
         while((*tomArgs).turn == 0) {
             pthread_cond_wait(&(*tomArgs).tom, &(*tomArgs).lock);
         }
-        (*tomArgs).stonesAvailable -= rand() % 3 + 1;
-        printf("Tom Thread stones: %d\n",(*tomArgs).stonesAvailable);
+        if ((*tomArgs).stonesAvailable <= 0) {
+            printf("Tom is the winner!\n");
+            pthread_exit(0);
+        }
+        srand(time(NULL));
+        stones = rand() % 4 + 1;
+        if ((*tomArgs).stonesAvailable >= stones) {
+            (*tomArgs).stonesAvailable -= stones;
+            printf("Tom picks up %d stones, %d left\n\n",stones, (*tomArgs).stonesAvailable);
+        } else if ((*tomArgs).stonesAvailable < stones) {
+            printf("Tom picks up %d stones, %d left\n\n",(*tomArgs).stonesAvailable, 0);
+            (*tomArgs).stonesAvailable -= (*tomArgs).stonesAvailable;
+        }
         (*tomArgs).turn = 0;
         pthread_cond_signal(&(*tomArgs).jerry);
-        sleep(1);
+        //sleep(1);
         pthread_mutex_unlock(&(*tomArgs).lock);
     }
 
@@ -65,8 +74,17 @@ void* tomTurn(void* arg) {
 int main(int argc, char *argv[]) {
     struct threadArgs *share;
     share = malloc(sizeof(struct threadArgs));
+    srand(time(NULL));
     (*share).stonesAvailable = rand() % 20 + 80;
+
+    printf("amount of stones: %d\n", (*share).stonesAvailable);
+
     (*share).turn = 1;
+
+    pthread_mutex_init(&(*share).lock, NULL);
+    pthread_cond_init(&(*share).jerry, NULL);
+    pthread_cond_init(&(*share).tom, NULL);
+
     pthread_t jerryID, tomID;
     pthread_attr_t attr;
     pthread_attr_init(&attr);
